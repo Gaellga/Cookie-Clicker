85f1d75ed98c797435736ee5b024d25357d9010b
Gaelle Gaban <gaellegaban@gmail.com>
2019-02-07 15:48:56 +0100

    autoclick fonctionnel

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,13 @@
-<<<<<<< HEAD
-var click=0;
-var score = 201;
-
-setInterval(function(){
-    if (score >= 200) {
-        click++;
-=======
 var score = 0;
  
 document.getElementById('clic').addEventListener('click', function (){
 score =score+1;
 document.getElementById('affichage').innerHTML = "Score:" + score;
-
  });
 
-
 setInterval(function(){
-    if (score >= 200) {
-        score++;
->>>>>>> a70f5302753484430948ce6b37425c647fcfdefe
-    };
-    console.log(click);
+        if (score >= 20) {
+            score++;
+        };
+    document.getElementById('affichage').innerHTML = "Score:" + score;
 }, 1000);
-
-console.log(click);
-
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> a70f5302753484430948ce6b37425c647fcfdefe
