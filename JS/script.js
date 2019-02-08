7087a5f35622a9a8db4847f65b77122454a26721
Your Name <you@example.com>
2019-02-08 09:59:41 +0100

    avancé

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,36 @@
 
-var score=49;
-var prix=50;
-var multiplicateur=0;
 
+  var score =0;
+  var prix = 50;
+  var multiplicateur=0;
 
-  document.getElementById("clic").addEventListener("click", function() {
-    score++;
-    score+=multiplicateur
-document.getElementById("affichage").innerHTML=score;
-  });
+  document.getElementById('clic').addEventListener('click', function (){
+      score++;
+      score+=multiplicateur;
+  document.getElementById('affichage').innerHTML = "Score:" + score;
+   });
 
-document.getElementById("multiplier").addEventListener("click", function augmenterMultiplicateur() {
+  function buyAutoclick() {
+      if (score >= 10) {
+              score = score - 10;
+              setInterval(function(){
+                  if (score >= 20) {
+                      score++;
+                  };
+                  document.getElementById('affichage').innerHTML = "Score:" + score;
+              }, 1000);
+          };
+  }
 
-if (score>=50) {
-multiplicateur++;
-score = score -(prix * multiplicateur);
-document.getElementById("affichage-prix").innerHTML= "-50";
-}
-else {
-  alert("pas assez");
-}
+  document.getElementById("multiplier").addEventListener("click", function augmenterMultiplicateur() {
+
+      if (score>=50) {
+      multiplicateur++;
+      score = score -(prix * multiplicateur);
+      var x = multiplicateur+1
+      document.getElementById("multiplier").innerHTML= "Multiplier x "+x;
+      }
+      else {
+        alert("pas assez");
+      }
   });
