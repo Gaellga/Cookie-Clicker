7fc59fa6f7a1b73594b9eaa2a31af71ef4e9a462
dorianbec <dorian@soloxs.com>
2019-02-12 09:15:11 +0100

    commit bonus

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,7 @@ var prix = 50;
 var multiplicateur=0;
 var currenttime = new Date();
 var currentsec = currenttime.getSeconds();
-var prixAuto = 500;
+var prixAuto = 5;
 var click = 1;
 //clic cookie
 function clic() {
@@ -20,6 +20,7 @@ document.getElementById('clic').addEventListener('click', function depclick(){
 });
 document.getElementById('clic').removeEventListener('click', function depclick(){
 });
+
  //multiplicateur
 document.getElementById("multiplier").addEventListener("click", function augmenterMultiplicateur() {
 
@@ -42,11 +43,11 @@ function buyAutoclick() {
  if (alreadyPlayed === false && score-prixAuto<0){
         console.log("You have to create more cookies first!");
       }
-      else {
-        if (alreadyPlayed=== false && score >= 500) {
-            score = score - 500;
+ else {
+        if (alreadyPlayed === false && score >= prixAuto) {
+            score = score - prixAuto;
             setInterval(function(){
-                if (score >= 200) {
+                if (score >= 2) {
                     clic();
                 };
                 alreadyPlayed = true;  
@@ -61,22 +62,26 @@ document.getElementById("bonus").addEventListener("click", function() {
 		if(score > 1){
 			var sec = 10;
 			score = score - 1;
+			var cli = click
 			console.log(click +"dep");
-			document.getElementById('clic').addEventListener('click', function click(){
-			clic();	
+			document.getElementById('clic').addEventListener('click', function click(){		
+			
+			clic();
+			
 			});
 			var interval = setInterval(function() {
 				sec--;
 				document.getElementById("bonus").className = "nobonus";
 				document.getElementById("bonus").innerHTML = sec;
+				
 				console.log(click +"bcl");
 				if(sec === 0) {
 					clearInterval(interval);
 					document.getElementById("bonus").innerHTML = "Get Bonus!";
       				document.getElementById("bonus").className = "bonus";
-      				console.log(click +"bclif");
+      				console.log(click +"bcl");
 				}
-				console.log(click +" fin");
+				console.log(click + " fin");
 			}, 1000);
 		}		
 		else {
@@ -84,4 +89,6 @@ document.getElementById("bonus").addEventListener("click", function() {
     	}
     	document.getElementById('clic').removeEventListener('click', function click(){
 			});   
+});
+document.getElementById("bonus").addEventListener("click", function() {
 });
